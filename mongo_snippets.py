1aac4a245e75ce555d0302e06b78c1e2e51ccb53
Tyler Brock <tyler.brock@gmail.com>
2012-08-24 17:57:05 -0400

    bs

--- mongo_snippets.py
+++ mongo_snippets.py
@@ -5,10 +5,15 @@ from flask import render_template
 
 mongo_snippets = Flask(__name__)
 
-structure = {}
-
 SNIPPET_BASE = './snippets'
 
+LANGS = {
+  "js": "JavaScript",
+  "rb": "Ruby",
+  "py": "Python"
+}
+
+"""
 for category in os.listdir(SNIPPET_BASE):
     category_path = os.path.join(SNIPPET_BASE, category)
     snippet_list = os.listdir(category_path)
@@ -16,10 +21,36 @@ for category in os.listdir(SNIPPET_BASE):
     snippets = [' '.join(name.split('_')).title() for name in snippet_names]
 
     structure[category] = snippets
+"""
 
-@mongo_snippets.route("/")
-def index():
-    return render_template("index.html", structure=structure)
+def path(dirs):
+  return os.path.join(SNIPPET_BASE, *dirs)
+
+def make_snippet(category, name):
+    snippet = {}
+    snippet['languages'] = {}
+    for phile in os.listdir(path([category])):
+	file_name, ext = phile.split('.')
+        if file_name == name:
+            data = open(path([category, phile])).read()
+            if ext == 'txt':
+                snippet['description'] = data
+            else:
+                lang = LANGS[ext]
+	        snippet['languages'][lang] = data
+
+    snippet['name'] = ' '.join(name.split('_')).title()
+    return snippet
+
+@mongo_snippets.route("/snippet/<category>/<snippet>")
+def show_snippet(category, snippet):
+    snippet = make_snippet(category, snippet)
+    return render_template("one_snippet.html", snippet=snippet)
+
+@mongo_snippets.route("/<category>/<snippet>")
+def index(category, snippet):
+    snippet = make_snippet(category, snippet)
+    return render_template("index.html", structure={}, snippet=snippet)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
