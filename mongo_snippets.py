e67fc270d7ffb7dc91c4686a3f756f9f82951374
Mike O'Brien <mikeo@10gen.com>
2012-09-25 12:11:23 -0400

    create snippets stiuff working

--- mongo_snippets.py
+++ mongo_snippets.py
@@ -1,8 +1,9 @@
 import os
 from sets import Set
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from pymongo import Connection
+from bson import ObjectId
 
 mongo_snippets = Flask(__name__)
 
@@ -13,13 +14,41 @@ conn = Connection(mongolab_uri)
 db = conn['mongo-snippets']
 snippets = db['snippets']
 
-@mongo_snippets.route('/snippet')
+langs = [ "C", "C#", "C++", "Java", "NodeJS",
+          "MongoShell", "Perl", "PHP",
+           "Python", "Ruby", "Scala", "Go"]
+
+@mongo_snippets.route('/snippet', methods=['GET','POST'])
 def new_snippet():
     error = None
     if request.method == 'POST':
-        snippets.insert(request.form['snippet'])
+        code = request.form.get('snippet_code', "")
+        lang = request.form.get('language', "")
+        name = request.form.get('name', "")
+        desc = request.form.get('description', "")
+        #TODO validate language, name, description
+        snippet = dict(code=code, lang=lang, name=name, desc=desc)
+        oid = snippets.insert(snippet, safe=True)
+        return redirect(url_for("show_snippet", snip_id=str(oid)))
     else:
-        return render_template('create_snippet.html')
+        return render_template('create_snippet.html', langs=langs)
+
+@mongo_snippets.route('/snippet/<snip_id>', methods=['GET','POST'])
+def show_snippet(snip_id):
+    try:
+        snip_id_oid = ObjectId(snip_id)
+    except:
+        snip_id_oid = None
+
+    if not snip_id_oid:
+        abort(404)
+
+    snippet = snippets.find_one({"_id" : snip_id_oid})
+    if not snippet:
+        abort(404)
+    else:
+        return render_template("snippet.html", snippet=snippet)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
