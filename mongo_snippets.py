08efa0654948b34ef7e341e85d66559e2b209679
Tyler Brock <tyler.brock@gmail.com>
2012-09-25 11:05:09 -0400

    strippin

--- mongo_snippets.py
+++ mongo_snippets.py
@@ -6,57 +6,15 @@ from pymongo import Connection
 
 mongo_snippets = Flask(__name__)
 
-SNIPPET_BASE = './snippets'
+default_uri = 'mongodb://localhost:27017/snippets'
+mongolab_uri = os.env("MONGOLAB_URI", None) or default_uri
 
-LANGS = {
-  "js": "JavaScript",
-  "rb": "Ruby",
-  "py": "Python"
-}
+conn = Connection(mongolab_uri)
+db = conn['snippets']
 
-"""
-for category in os.listdir(SNIPPET_BASE):
-    category_path = os.path.join(SNIPPET_BASE, category)
-    snippet_list = os.listdir(category_path)
-    snippet_names = Set([snippet.split('.')[0] for snippet in snippet_list])
-    snippets = [' '.join(name.split('_')).title() for name in snippet_names]
-
-    structure[category] = snippets
-"""
-
-
-mongolab_uri = os.env("MONGOLAB_URI", None) or 'mongodb://localhost:27017/snippets'
-c = Connection(mongo_url)
-db = c['snippets']
-
-def path(dirs):
-  return os.path.join(SNIPPET_BASE, *dirs)
-
-def make_snippet(category, name):
-    snippet = {}
-    snippet['languages'] = {}
-    for phile in os.listdir(path([category])):
-	file_name, ext = phile.split('.')
-        if file_name == name:
-            data = open(path([category, phile])).read()
-            if ext == 'txt':
-                snippet['description'] = data
-            else:
-                lang = LANGS[ext]
-	        snippet['languages'][lang] = data
-
-    snippet['name'] = ' '.join(name.split('_')).title()
-    return snippet
-
-@mongo_snippets.route("/snippet/<category>/<snippet>")
-def show_snippet(category, snippet):
-    snippet = make_snippet(category, snippet)
-    return render_template("one_snippet.html", snippet=snippet)
-
-@mongo_snippets.route("/<category>/<snippet>")
-def index(category, snippet):
-    snippet = make_snippet(category, snippet)
-    return render_template("index.html", structure={}, snippet=snippet)
+@mongo_snippets.route('/snippet/new')
+def new_snippet():
+    pass
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
